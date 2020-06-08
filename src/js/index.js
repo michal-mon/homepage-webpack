91da15ba8e09055983c30ec0cc2c1fc20ec77388
Michal Mon <mmon1235@gmail.com>
2020-06-08 22:20:01 +0200

    dark mode & css custom properties added

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import moment from 'moment';
 
 /*const name = "Michał Moń";
 const age = 19;
@@ -14,6 +15,9 @@ const user = {
     ageUser: 19,
 }
 
+const godzina = moment().startOf('day').fromNow();
+console.log(`Dzień zaczął się: ${godzina}`);
+
 function helloWorld(name, age, className){
 console.log(`Cześć, jestem ${name} i mam ${age} lat, a ta funkcja została wywołana z obiektu klasy ${className}`);
 }
@@ -25,5 +29,23 @@ const hamburger = document.querySelector(".hamburger--js");
 hamburger.addEventListener('click', () =>{
     const nav = document.querySelector('.navigation--js');
     nav.classList.toggle('navigation--open');
+});
+
+
+let isDark = false;
+const darkButton = document.querySelector(".dark-mode");
+darkButton.addEventListener('click', ()=>{
+    
+    if (isDark == false){
+        isDark = true;
+        document.documentElement.style.setProperty('--text-color', 'white');
+        document.documentElement.style.setProperty('--background-color', 'black');
+    }
+    else{
+        isDark = false;
+        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--background-color', '-webkit-linear-gradient(to right, #D7DDE8, #757F9A)');
+    }
+
 });
 
